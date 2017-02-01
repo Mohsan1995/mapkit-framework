2025cf73f31be1debdc8f089bc2370b7197c0676
CrabeMan MacBook <q_leprat@yahoo.fr>
2017-02-01 15:19:16 +0100

    Hash fix

--- Sample/City.swift
+++ Sample/City.swift
@@ -29,7 +29,7 @@ class City: Annotationable {
     }
     
     override var hashValue: Int {
-        return name.hashValue * country.hashValue
+        return name.hash
     }
     
     static func == (lhs: City, rhs: City) -> Bool {
