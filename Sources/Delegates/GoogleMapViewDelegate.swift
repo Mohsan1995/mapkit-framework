57814d5f1b4b8ff03ceddb89f5d90326652ca604
CrabeMan MacBook <q_leprat@yahoo.fr>
2017-02-12 16:52:52 +0100

    Select|Unselect Google delegate

--- Sources/Delegates/GoogleMapViewDelegate.swift
+++ Sources/Delegates/GoogleMapViewDelegate.swift
@@ -2,5 +2,29 @@ import UIKit
 import GoogleMaps
 
 class GoogleMapViewDelegate: ApiMapViewDelegate, GMSMapViewDelegate {
-
+    var markerSelected: GMSMarker?
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        if markerSelected != nil {
+            if let annotation = controller.getAnnotationByCoor(coor: markerSelected!.position) {
+                controller.delegate?.onUnselectAnnotation(annotation: annotation)
+            }
+        }
+        markerSelected = marker
+        
+        if let annotation = controller.getAnnotationByCoor(coor: marker.position) {
+            controller.delegate?.onSelectAnnotation(annotation: annotation)
+        }
+        
+        return false
+    }
+    
+    func mapView(_ mapView: GMSMapView, didCloseInfoWindowOf marker: GMSMarker) {
+        if markerSelected == marker {
+            if let annotation = controller.getAnnotationByCoor(coor: marker.position) {
+                controller.delegate?.onUnselectAnnotation(annotation: annotation)
+            }
+            markerSelected = nil
+        }
+    }
 }
